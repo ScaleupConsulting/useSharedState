5933a6ba9232fbea151f4f57dceb3cc1fd8b3e7f
Ahmet Can ACAR <ahmetcanacar93@gmail.com>
2022-06-07 17:34:16 +0300

    initial

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+export default class SharedStateTarget {
+  constructor(initialStateOfNewComponents: any);
+  initialStateOfNewComponents: any;
+  useSharedState(): [any, (detail: any) => any];
+}
+//# sourceMappingURL=index.d.ts.map
