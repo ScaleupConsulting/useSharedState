01e688dff5587f2a4f450d1c168722a94fcdd23f
luis.luft <luberluft@gmail.com>
2022-11-28 07:49:46 -0400

    so that the cjs file and js file are the exact same

--- index.cjs
+++ index.cjs
@@ -15,7 +15,18 @@ class SharedStateTarget extends EventTarget {
     const setSharedState = (detail) => super.dispatchEvent(new CustomEvent("set", { detail }));
 
     useEffect(() => {
-      const eventListener = ({ detail }) => setState((this.initialStateOfNewComponents = detail));
+      const eventListener = ({ detail }) => {
+        if (typeof detail === "function") {
+          setState((...args) => {
+            const nextState = detail(...args);
+            this.initialStateOfNewComponents = nextState;
+            return nextState;
+          });
+        } else {
+          this.initialStateOfNewComponents = detail;
+          setState(detail);
+        }
+      };
 
       super.addEventListener("set", eventListener);
       return () => super.removeEventListener("set", eventListener);
